ca457e40ae787eb2f6db4cf0cb42f22939a269a0
david rus <davidrus9@yahoo.com>
2024-11-10 16:45:38 +0200

    Lab5 terminat

--- Rus_David-Sorin_Lab2/Pages/Borrowings/Delete.cshtml.cs
+++ Rus_David-Sorin_Lab2/Pages/Borrowings/Delete.cshtml.cs
@@ -29,7 +29,12 @@ namespace Rus_David_Sorin_Lab2.Pages.Borrowings
                 return NotFound();
             }
 
-            var borrowing = await _context.Borrowing.FirstOrDefaultAsync(m => m.ID == id);
+            var borrowing = await _context.Borrowing
+                .Include(b => b.Member)
+                .Include(b => b.Book)
+                .Include(b => b.Book.Author)
+                .Include(b => b.Book.Publisher)
+                .FirstOrDefaultAsync(m => m.ID == id);
 
             if (borrowing == null)
             {
