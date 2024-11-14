c9ad9bea14e895213d221c0eb64f66e9649a43cb
david rus <davidrus9@yahoo.com>
2024-11-14 18:45:54 +0200

    Lab6 complet

--- Rus_David-Sorin_Lab2/Pages/Books/Create.cshtml.cs
+++ Rus_David-Sorin_Lab2/Pages/Books/Create.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -10,6 +11,7 @@ using Rus_David_Sorin_Lab2.Models;
 
 namespace Rus_David_Sorin_Lab2.Pages.Books
 {
+    [Authorize(Roles = "Admin")]
     public class CreateModel : BookCategoriesPageModel
     {
         private readonly Rus_David_Sorin_Lab2Context _context;
